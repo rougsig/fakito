9b5a29325eae4cf4fb662968fece2171b8c80d25
st <rougsig@gmail.com>
2019-03-16 21:21:18 +0200

    Rename TestNothing

--- processor/src/test/kotlin/TestMockViewGenerator.kt
+++ processor/src/test/kotlin/TestMockViewGenerator.kt
@@ -1,6 +1,6 @@
 package com.github.rougsig.mviautomock.processor
 
-class TestNothing : APTest("com.example.mvi") {
+class TestMockViewGenerator : APTest("com.example.mvi") {
   fun testNothing() {
     testProcessor(AnnotationProcessor(
       sourceFiles = listOf("LoginView.java"),
